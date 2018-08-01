defad2fdc5927a116151077f1b4bc2582ea53eab
TomasStep <ttomass@gmail.com>
2018-08-01 14:04:54 +0300

    code v2

--- Program.cs
+++ Program.cs
@@ -15,37 +15,40 @@ namespace MDPyramid
                 return false;
             }
         }
+        static void infoPrint(bool e, int n, long s, int p)
+        {
+            if(e) Console.WriteLine($"{n} is even");
+            else Console.WriteLine($"{n} is  not even");
+            Console.WriteLine($"Current sum is {s}");
+            Console.WriteLine($"Current position is {++p}");
+        }
         static void Main(string[] args)
         {
             int start = 0; //the position where binary tree starts
-            int pos = start;
-            long sum = 0;
-            bool even = false;
+            int pos = start; //the current position to find it childs
+            long sum = 0; //sum of binary tree
+            bool even = false; //a bool
             int num;
             int comp1, comp2;
-            using (TextReader reader = File.OpenText("sample.txt"))
+            using (TextReader reader = File.OpenText("samples/advancedsample.txt"))
             {
                 string datasample;
                 while ((datasample = reader.ReadLine()) != null)
                 {
                     string[] bits = datasample.Split(' ');
                     int len = bits.Length;
-                    Console.WriteLine($"len = {len}");
+                    Console.WriteLine($"Line length = {len}");
                     if(len == 1)
                     {
                         num = Convert.ToInt32(bits[pos]);
                         even = isEven(num);
-                        if(even) Console.WriteLine($"{num} is even");
-                        else Console.WriteLine($"{num} is  not even");
                         sum += num;
-                        Console.WriteLine($"sum = {sum}");
+                        infoPrint(even, num, sum, pos);
                     }else
                     {
                         comp1 = Convert.ToInt32(bits[pos]);
                         comp2 = Convert.ToInt32(bits[pos+1]);
-                        if(even) Console.WriteLine("last number even");
-                        else Console.WriteLine("last number !even");
-                        Console.WriteLine($"here, comp1 = {comp1} and comp2 = {comp2}");
+                        Console.WriteLine($"Childs for compare is {comp1} and {comp2}");
                         if(!even)
                         {
                             if(comp1 % 2 == 0 && comp2 % 2 == 0)
@@ -54,28 +57,26 @@ namespace MDPyramid
                                 {
                                     sum += comp1;
                                     even = isEven(comp1);
-                                    Console.WriteLine($"sum = {sum}, pos = {pos}");
+                                    infoPrint(even, comp1, sum, pos);
                                 }else
                                 {
                                     sum += comp2;
                                     pos++;
                                     even = isEven(comp2);
-                                    Console.WriteLine($"sum = {sum}, pos = {pos}");
+                                    infoPrint(even, comp2, sum, pos);
                                 }
                             }else if(comp1 % 2 == 0)
                             {
                                 sum += comp1;
                                 even = isEven(comp1);
-                                Console.WriteLine($"sum = {sum}, pos = {pos}");
+                                infoPrint(even, comp1, sum, pos);
                             }else if(comp2 % 2 == 0)
                             {
                                 sum += comp2;
                                 pos++;
                                 even = isEven(comp2);
-                                Console.WriteLine($"sum = {sum}, pos = {pos}");
+                                infoPrint(even, comp2, sum, pos);
                             }
-                            //bits[next] bits[next+1]
-
                         }else
                         {
                             if(comp1 % 2 != 0 && comp2 % 2 != 0)
@@ -84,32 +85,31 @@ namespace MDPyramid
                                 {
                                     sum += comp1;
                                     even = isEven(comp1);
-                                    Console.WriteLine($"sum = {sum}, pos = {pos}");
+                                    infoPrint(even, comp1, sum, pos);
                                 }else
                                 {
                                     sum += comp2;
                                     pos++;
                                     even = isEven(comp2);
-                                    Console.WriteLine($"sum = {sum}, pos = {pos}");
+                                    infoPrint(even, comp2, sum, pos);
                                 }
                             }else if(comp1 % 2 != 0)
                             {
                                 sum += comp1;
                                 even = isEven(comp1);
-                                Console.WriteLine($"sum = {sum}, pos = {pos}");
+                                infoPrint(even, comp1, sum, pos);
                             }else if(comp2 % 2 != 0)
                             {
                                 sum += comp2;
                                 pos++;
                                 even = isEven(comp2);
-                                Console.WriteLine($"sum = {sum}, pos = {pos}");
+                                infoPrint(even, comp2, sum, pos);
                             }
                         }
-                        //start = next;
                     }   
-                    Console.WriteLine("-------------------------");     
+                    Console.WriteLine("------------------------------");     
                 }
-                Console.WriteLine($"sum = {sum}");
+                Console.WriteLine($"The final output of Sum is {sum}");
             }
         }
     }
